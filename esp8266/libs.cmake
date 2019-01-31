cfc222321bf94e600456bb1e1357ee7c7df11418
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2019-01-31 16:51:10 +0200

    more esp8266 wifi sources

--- esp8266/libs.cmake
+++ esp8266/libs.cmake
@@ -6,6 +6,8 @@ set(wifi_sources
             ${WIFI_ESP8266_DIR}/ESP8266WiFi.cpp
             ${WIFI_ESP8266_DIR}/WiFiClient.cpp
             ${WIFI_ESP8266_DIR}/ESP8266WiFiMulti.cpp
+            ${WIFI_ESP8266_DIR}/ESP8266WiFiScan.cpp
+            ${WIFI_ESP8266_DIR}/ESP8266WiFiAP.cpp
 )
 set(wire_sources
   ${WIRE_ESP8266_DIR}/Wire.cpp
