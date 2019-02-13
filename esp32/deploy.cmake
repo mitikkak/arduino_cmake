3e22214e1a4482c88105e5b33eade88e83d05f03
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2019-02-13 16:29:10 +0200

    esp32 builds when SDK is fetched: 1) cloning of https://github.com/espressif/arduino-esp32.git 2) git submodule sync's and updates 3) cd tools && python3 tools/get.py

--- esp32/deploy.cmake
+++ esp32/deploy.cmake
@@ -1,5 +1,5 @@
 function(partitions NAME)
-    add_custom_target(partitions_${NAME} COMMAND python ${ESP32_PATH}/hardware/esp32/1.0.0-rc4/tools/gen_esp32part.py -q ${ESP32_PATH}/hardware/esp32/1.0.0-rc4/tools/partitions/default.csv ${NAME}.partitions.bin)
+    add_custom_target(partitions_${NAME} COMMAND python ${ESP32_PATH}/tools/gen_esp32part.py -q ${ESP32_PATH}/tools/partitions/default.csv ${NAME}.partitions.bin)
 endfunction()
 
 function(elf_to_bin_wroom32 NAME)
@@ -11,6 +11,6 @@ endfunction()
 function(upload_wroom32 NAME)
     add_custom_target(upload_wroom32_${NAME}
         COMMAND
-        python ${ESP32_PATH}/tools/esptool/2.3.1/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ${ESP32_PATH}/hardware/esp32/1.0.0-rc4/tools/partitions/boot_app0.bin 0x1000 ${ESP32_PATH}/hardware/esp32/1.0.0-rc4/tools/sdk/bin/bootloader_dio_80m.bin 0x10000 ${NAME}.bin 0x8000 ${NAME}.partitions.bin 
+        python ${ESP32_PATH}/tools/esptool/2.3.1/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0xe000 ${ESP32_PATH}/tools/partitions/boot_app0.bin 0x1000 ${ESP32_PATH}/tools/sdk/bin/bootloader_dio_80m.bin 0x10000 ${NAME}.bin 0x8000 ${NAME}.partitions.bin 
     )
 endfunction()
