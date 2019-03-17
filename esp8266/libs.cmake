c05596b702d202766659b02ccfadbde45c614598
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2019-03-17 21:38:29 +0200

    LiquidCrystal_I2C stub, esp8266 web server sources

--- esp8266/libs.cmake
+++ esp8266/libs.cmake
@@ -10,6 +10,10 @@ set(wifi_sources
             ${WIFI_ESP8266_DIR}/ESP8266WiFiAP.cpp
             ${WIFI_ESP8266_DIR}/WiFiUdp.cpp
 )
+set(esp8266_webserver_sources
+    ${WEB_SERVER_ESP8266_DIR}/ESP8266WebServer.cpp
+    ${WEB_SERVER_ESP8266_DIR}/Parsing.cpp
+)
 set(wire_sources
   ${WIRE_ESP8266_DIR}/Wire.cpp
 )
---
+++ ut/stubs/LiquidCrystal_I2C.h
@@ -0,0 +1,8 @@
+
+#pragma once
+
+class LiquidCrystal_I2C
+{
+public:
+    LiquidCrystal_I2C(int, int, int){}
+};
