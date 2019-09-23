12d3956e3a2eaf3b56f5b049291c0a5c5cd627f7
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2019-09-23 21:35:23 +0300

    blink generic fails at linker

---
+++ esp8266/eagle.cmake
@@ -0,0 +1,11 @@
+
+set(eagle_output local.eagle.app.v6.common.ld)
+set(eagle_input ${ESP8266_PATH}/tools/sdk/ld/eagle.app.v6.common.ld.h)
+add_custom_command(
+  OUTPUT ${eagle_output}
+  #BUILD_EXTRA_FLAGS="-DNDEBUG -DVTABLES_IN_FLASH"
+  #$ESP_ROOT/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-gcc -CC -E -P $BUILD_EXTRA_FLAGS $ESP_ROOT/tools/sdk/ld/eagle.app.v6.common.ld.h -o $ESP_ROOT/tools/sdk/ld/eagle.app.v6.common.ld
+  COMMAND "${CMAKE_C_COMPILER}" -CC -E -P "-DNDEBUG -DVTABLES_IN_FLASH" ${eagle_input} -o ${eagle_output}
+  DEPENDS ${eagle_input}
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+)
