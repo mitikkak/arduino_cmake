953306272a839f27c2f79d036f029c82860416fa
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2019-09-24 15:07:30 +0300

    eagle file to generic.cmake

--- esp8266/generic.cmake
+++ esp8266/generic.cmake
@@ -3,10 +3,11 @@ include(${ARDUINO_CMAKE_DIR}/esp8266/libs.cmake)
 include(${ARDUINO_CMAKE_DIR}/esp8266/tools.cmake)
 include(${ARDUINO_CMAKE_DIR}/esp8266/defs.cmake)
 include(${ARDUINO_CMAKE_DIR}/esp8266/deploy.cmake)
+include(${ARDUINO_CMAKE_DIR}/esp8266/eagle.cmake)
 include(${ARDUINO_CMAKE_DIR}/esp8266/env/wifi.cmake)
 
 function(build_targets_esp8266_generic sources_to_build external_include_dirs)
-add_executable(${PROJECT_NAME} ${sources_to_build})
+add_executable(${PROJECT_NAME} ${sources_to_build} ${eagle_output})
 target_link_libraries(${PROJECT_NAME} PRIVATE ${esp8266_linker_options})
 add_dependencies(${PROJECT_NAME} ${esp8266_core_libs})
 target_compile_options(
