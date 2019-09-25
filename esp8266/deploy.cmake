57220a45c09a68a05738134984055f200b8747ff
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2019-09-25 21:08:44 +0300

    blink generic deployment working

--- esp8266/deploy.cmake
+++ esp8266/deploy.cmake
@@ -1,18 +1,18 @@
 
 function(elf_to_bin NAME)
-add_custom_target(elf_to_bin_${NAME} COMMAND "${ESP8266_PATH}/tools/esptool/0.4.12/esptool" -eo "${ESP8266_PATH}/hardware/esp8266/2.4.0/bootloaders/eboot/eboot.elf" -bo "${NAME}.bin" -bm qio -bf 40 -bz 512K -bs .text -bp 4096 -ec -eo "${NAME}" -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec)
+add_custom_target(elf_to_bin_${NAME} COMMAND COMMAND python "${ESP8266_PATH}/tools/elf2bin.py" --eboot "${ESP8266_PATH}/bootloaders/eboot/eboot.elf" --app "${NAME}" --flash_mode dout --flash_freq 40 --flash_size 512K --path "${ESP8266_PATH}/tools/xtensa-lx106-elf/bin" --out "${NAME}.bin")
 endfunction()
 
 function(elf_to_bin_esp8266_12e NAME)
-add_custom_target(elf_to_bin_esp8266_12e_${NAME} COMMAND "${ESP8266_PATH}/tools/esptool/0.4.12/esptool" -eo "${ESP8266_PATH}/hardware/esp8266/2.4.0/bootloaders/eboot/eboot.elf" -bo "${NAME}.bin" -bm dio -bf 40 -bz 4M -bs .text -bp 4096 -ec -eo "${NAME}" -bs .irom0.text -bs .text -bs .data -bs .rodata -bc -ec)
+add_custom_target(elf_to_bin_esp8266_12e_${NAME} COMMAND python "${ESP8266_PATH}/tools/elf2bin.py" --eboot "${ESP8266_PATH}/bootloaders/eboot/eboot.elf" --app "${NAME}" --flash_mode dio --flash_freq 40 --flash_size 4M --path "${ESP8266_PATH}/tools/xtensa-lx106-elf/bin" --out "${NAME}.bin")
 endfunction()
 
 function(upload NAME DEPS)
-add_custom_target(upload_${NAME} COMMAND ${ESP8266_PATH}/tools/esptool/0.4.12/esptool -vv -cd ck -cb 115200 -cp /dev/ttyUSB0 -ca 0x00000 -cf ${NAME}.bin 
+add_custom_target(upload_${NAME} COMMAND python "${ESP8266_PATH}/tools/esptool/esptool.py" --chip esp8266 --port "/dev/ttyUSB0" --baud "115200" --before no_reset --after soft_reset write_flash 0x0 "${NAME}.bin"
                                  DEPENDS ${DEPS})
 endfunction()
 
 function(upload_esp8266_12e NAME DEPS)
-add_custom_target(upload_esp8266_12e_${NAME} COMMAND ${ESP8266_PATH}/tools/esptool/0.4.12/esptool -vv -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x00000 -cf ${NAME}.bin 
+add_custom_target(upload_esp8266_12e_${NAME} COMMAND python "${ESP8266_PATH}/tools/upload.py" --chip esp8266 --port "/dev/ttyUSB0" --baud "460800"  write_flash 0x0 "${NAME}.bin" --end
                                  DEPENDS ${DEPS})
 endfunction()
