84b2a80caa37f831e2dec63a4c715b3f1f4e646d
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2019-10-09 16:11:21 +0300

    adding math.h and some macros, needed by TinyGPSPlus

--- ut/stubs/Arduino.h
+++ ut/stubs/Arduino.h
@@ -4,6 +4,7 @@
 //#include <assert.h>
 #include "gtest/gtest.h"
 #include <cstdint>
+#include <math.h>
 //#include "TinyDebugSerial.h"
 #include "Serial.h"
 #include "PinEvents.h"
@@ -42,4 +43,12 @@ extern void delay(int const);
 //extern TinyDebugSerial Serial;
 
 #define PI 3.1415926535897932384626433832795
+#define TWO_PI 6.283185307179586476925286766559
 
+#define DEG_TO_RAD 0.017453292519943295769236907684886
+#define radians(deg) ((deg)*DEG_TO_RAD)
+
+#define RAD_TO_DEG 57.295779513082320876798154814105
+#define degrees(rad) ((rad)*RAD_TO_DEG)
+
+#define sq(x) ((x)*(x))
